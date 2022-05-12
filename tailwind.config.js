cbbcc29d842ce970bbfb7ea988f0fa5fa8750700
Toriqul-Leon <yeitoriqul@gmail.com>
2022-05-12 23:31:16 +0600

    Minimal setup Ready

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0fcfec",
+          secondary: "#19d3ae",
+          accent: "#3a4256",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
